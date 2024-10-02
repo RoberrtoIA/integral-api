395c303b53c819eba5642ca3e99d7eb0294e15d4
Roberto Moreno <robertoantoniomoreno1999@gmail.com>
2024-10-01 20:43:38 -0600

    Swagger configuracion inicial

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 import express from 'express'
-import userRoutes from './routes/users.routes.js'
-import taskRoutes from './routes/tasks.routes.js'
+import swaggerUI from 'swagger-ui-express'
+import swaggerJSdoc from 'swagger-jsdoc'
+import userRoutes from './V1/routes/users.routes.js'
+import taskRoutes from './V1/routes/tasks.routes.js'
 
 const app = express()
 
-// app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-
 app.use('/api/v1/usuarios', userRoutes);
 app.use('/api/v1/tareas', taskRoutes);
-// app.use(userRoutes);
-// app.use(taskRoutes);
+
+const swaggerSpec = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Integral API REST", version: "1.0.0',
+            version: '1.0.0',
+        }
+    },
+    apis: ['./api/v1/routes/*.js'],
+}
+
+
+app.use('/api/v1/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerJSdoc(swaggerSpec)))
 
 export default app
