b44cde9afacf29c25640b639eed1d1cd68bb2faa
Roberto Moreno <robertoantoniomoreno1999@gmail.com>
2024-10-02 17:39:16 -0600

    sh script

--- init/init_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Esperar a que el servidor SQL esté listo
-sleep 30
-
-# Ejecutar el script SQL
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "C0mpl3x!Password#2024" -i /init.sql
